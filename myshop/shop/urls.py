e5025ad1fbd7b2baa2dc797eaa1bb328956fa5b4
akashgiricse <akashgiricse@gmail.com>
2018-12-12 21:35:39 +0530

    Add template

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list,
          name='product_list_by_category'),
-    path('<ind:id>/<slug:slug>/', views.product_detail,
+    path('<int:id>/<slug:slug>/', views.product_detail,
          name='product_detail'),
 
 ]
